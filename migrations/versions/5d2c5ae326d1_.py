ee3efbf0314eeb1ebffefda957f29990f9dcfa12
Makayla Jameson <makayla.jameson01@gmail.com>
2023-08-08 23:05:42 -0500

    fixed indent

--- migrations/versions/5d2c5ae326d1_.py
+++ migrations/versions/5d2c5ae326d1_.py
@@ -68,7 +68,7 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
-     if environment == "production":
+    if environment == "production":
         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
 
 
