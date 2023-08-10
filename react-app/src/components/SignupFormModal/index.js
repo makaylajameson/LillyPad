25c8631661d42e25c1e454324f3a9eee5e8831cd
Makayla Jameson <makayla.jameson01@gmail.com>
2023-08-10 17:21:35 -0500

    fixed front-end errors on sign up

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -14,30 +14,31 @@ function SignupFormModal() {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
 	const [errors, setErrors] = useState([]);
+	const [frontendErrors, setFrontendErrors] = useState({})
 	const { closeModal } = useModal();
 
 	useEffect(() => {
-		const errors = {}
+		const frontendErrors = {}
 
-		if (username.length < 4 || username.length > 30) errors["username"] = "Username must be between 4-30 characters"
-		if (!username.length) errors["username"] = "Username is required"
-		if (!firstName.length) errors["firstName"] = "First name is required"
-		if (firstName.length > 30 || firstName.length < 2) errors["firstName"] = "First name must be between 2-30 characters"
-		if (!lastName.length) errors["lastName"] = "Last name is required"
-		if (lastName.length > 30 || lastName.length < 2) errors["lastName"] = "Last name must be between 2-30 characters"
-		if (!email.includes('@') || !email.includes('.')) errors["email"] = "Please include a valid email"
-		if (password.length < 6) errors["password"] = "Password must be at least 6 characters long."
-		if (password.length > 30) errors["password"] = "Password must be less than 30 characters"
-		if (password !== confirmPassword) errors["password"] = "Password and confirm password must match"
+		if (username.length < 4 || username.length > 30) frontendErrors["username"] = "Username must be between 4-30 characters"
+		if (!username.length) frontendErrors["username"] = "Username is required"
+		if (!firstName.length) frontendErrors["firstName"] = "First name is required"
+		if (firstName.length > 30 || firstName.length < 2) frontendErrors["firstName"] = "First name must be between 2-30 characters"
+		if (!lastName.length) frontendErrors["lastName"] = "Last name is required"
+		if (lastName.length > 30 || lastName.length < 2) frontendErrors["lastName"] = "Last name must be between 2-30 characters"
+		if (!email.includes('@') || !email.includes('.')) frontendErrors["email"] = "Please include a valid email"
+		if (password.length < 6) frontendErrors["password"] = "Password must be at least 6 characters long."
+		if (password.length > 30) frontendErrors["password"] = "Password must be less than 30 characters"
+		if (password !== confirmPassword) frontendErrors["password"] = "Password and confirm password must match"
 
-		setErrors(errors)
+		setFrontendErrors(frontendErrors)
 	}, [email, username, firstName, lastName, password, confirmPassword])
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		const formData = new FormData();
-		if (Object.values(errors).length) return
+		// if (Object.values(errors).length) return
 
 		formData.append("first_name", firstName);
 		formData.append("last_name", lastName);
@@ -64,11 +65,6 @@ function SignupFormModal() {
 		<>
 			<h1>Sign Up</h1>
 			<form onSubmit={handleSubmit} encType="multipart/form-data">
-				<ul>
-					{Object.values(errors).map((error, idx) => (
-						<li key={idx}>{error}</li>
-					))}
-				</ul>
 				<label>
 					First Name
 					<input
@@ -78,6 +74,7 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
+				{frontendErrors.firstName && firstName.length > 0 && <p className='on-submit-frontendErrors'>{frontendErrors.firstName}</p>}
 				<label>
 					Last Name
 					<input
@@ -87,6 +84,8 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
+				{frontendErrors.lastName && lastName.length > 0 && <p className='on-submit-frontendErrors'>{frontendErrors.lastName}</p>}
+
 				<label>
 					Email
 					<input
@@ -96,6 +95,7 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
+				{frontendErrors.email && email.length > 0 && <p className='on-submit-frontendErrors'>{frontendErrors.email}</p>}
 				<label>
 					Username
 					<input
@@ -105,6 +105,7 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
+				{frontendErrors.username && username.length > 0 && <p className='on-submit-frontendErrors'>{frontendErrors.username}</p>}
 				<label>
 					Profile Image
 					<input
@@ -122,6 +123,7 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
+				{frontendErrors.password && password.length > 0 && <p className='on-submit-frontendErrors'>{frontendErrors.password}</p>}
 				<label>
 					Confirm Password
 					<input
@@ -131,6 +133,12 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
+				{frontendErrors.confirmPassword && confirmPassword.length > 0 && <p className='on-submit-frontendErrors'>{frontendErrors.confirmPassword}</p>}
+				{/* <ul>
+					{errors.map((error, idx) => (
+						<li key={idx}>{error}</li>
+					))}
+				</ul> */}
 				<button type="submit">Sign Up</button>
 			</form>
 		</>
