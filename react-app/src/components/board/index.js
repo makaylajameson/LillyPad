3d20ac491aaec42471181e3c64f565f79be80f98
Makayla Jameson <makayla.jameson01@gmail.com>
2023-08-16 10:40:45 -0500

    added footer

--- react-app/src/components/board/index.js
+++ react-app/src/components/board/index.js
@@ -5,6 +5,7 @@ import { getUserBoardsThunk } from "../../store/board"
 import BoardCard from './BoardCard/index';
 import OpenModalButton from "../OpenModalButton";
 import CreateBoardModal from "../board/CreateBoardModal";
+import Footer from "../footer";
 
 const CurrentBoardsPage = () => {
 
@@ -21,33 +22,35 @@ const CurrentBoardsPage = () => {
     if (!currentBoards) return <h3>Loading...</h3>
 
     return (
-        <div>
-            <div className="board-page-title-container">
+        <>
+            <div className="board-page-container">
+                <div className="board-page-title-container">
 
+                    <h2 className="board-page-title">
+                        <div className="user-img-name">
+                            <img className="profile-pic-board" src={user?.profile_pic} />
+                            <div>{user?.first_name} {user?.last_name}'s workspace</div>
+                        </div>
+                        <div className="your-boards-title">
+                            <i className="far fa-user"></i> Your Boards
+                        </div>
+                    </h2>
+                </div>
 
-                <h2 className="board-page-title">
-                    <div className="user-img-name">
-                        <img className="profile-pic-board" src={user?.profile_pic} />
-                        <div>{user?.first_name} {user?.last_name}'s workspace</div>
-                    </div>
-                    <div className="your-boards-title">
-                        <i className="far fa-user"></i> Your Boards
-                    </div>
-                </h2>
-            </div>
-
-            <div>
-                <div className="board-card-container">
-                    {userBoardsArr.map((board) => <BoardCard board={board} key={board.id} />)}
-                    <div className="create-new-board-button">
-                        <OpenModalButton
-                            buttonText="Create new board"
-                            modalComponent={<CreateBoardModal />}
-                        />
+                <div>
+                    <div className="board-card-container">
+                        {userBoardsArr.map((board) => <BoardCard board={board} key={board.id} />)}
+                        <div className="create-new-board-button">
+                            <OpenModalButton
+                                buttonText="Create new board"
+                                modalComponent={<CreateBoardModal />}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            < Footer />
+        </>
     )
 }
 
---
+++ react-app/src/components/footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <div className='person-container'>
+                <p className='footer-title'>Built By: Makayla Jameson</p>
+                <div className='link-container'>
+                    <a target='_blank' href='https://www.linkedin.com/in/makayla-jameson-701b47164/'><i class="fab fa-linkedin"></i></a>
+                    <a target='_blank' href='https://github.com/makaylajameson'><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
