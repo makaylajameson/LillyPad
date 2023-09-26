9a7f2027f9dcc6ae4c3abedf5f2e7141dfc97167
Makayla Jameson <makayla.jameson01@gmail.com>
2023-09-26 16:48:17 -0500

    fixed seeds

--- app/seeds/lists.py
+++ app/seeds/lists.py
@@ -3,14 +3,16 @@ from sqlalchemy.sql import text
 
 
 def seed_lists():
-    list_1 = List(title="In Progress", board_id=1)
+    list_1 = List(title="Back Log", board_id=1)
     list_2 = List(title="Complete", board_id=1)
     list_3 = List(title="In Progress", board_id=2)
-    list_4 = List(title="Complete", board_id=2)
+    list_4 = List(title="Ready For Review", board_id=2)
+    list_5 = List(title="Ready to Style", board_id=2)
     db.session.add(list_1)
     db.session.add(list_2)
     db.session.add(list_3)
     db.session.add(list_4)
+    db.session.add(list_5)
     db.session.commit()
 
 
