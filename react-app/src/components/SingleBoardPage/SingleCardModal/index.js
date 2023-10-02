22a8ced3021e935fb681477286194b4ec5503235
Makayla Jameson <makayla.jameson01@gmail.com>
2023-10-02 09:57:56 -0500

    fixed card details and overflow issues

--- react-app/src/components/SingleBoardPage/SingleCardModal/index.js
+++ react-app/src/components/SingleBoardPage/SingleCardModal/index.js
@@ -1,15 +1,14 @@
-
+import './SingleCardModal.css';
 
 
 const SingleCardModal = ({ card }) => {
 
     return (
         <div className="single-card-background">
-            <div className="single-card-cover-img-preview" style={{ backgroundColor: card.cover_image }}></div>
             <div className='single-card-container'>
                 <h1 className="single-card-title">{card.title}</h1>
-                <p className='single-card-list-title'>in list {card.list_title}</p>
-                <p className='single-card-description-header'>Description</p>
+                <p className='single-card-list-title'>In List: {card.list_title}</p>
+                <p className='single-card-description-header'>Description: </p>
                 <p className='single-card-description'>{card.description}</p>
             </div>
         </div>
